0a18f6ff01fee69cd332fda7f3695cdeabe83362
Yair Quintana <yairquintanamx@gmail.com>
2024-02-19 06:25:36 -0600

    refactor(components): Correct modal design (23 min)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import './App.css'
 import Message from './components/Message'
 import Navbar from './components/Navbar'
 import BookTable, { BookProps } from './components/BookTable'
+import BookModal from './components/BookModal'
 
 function App() {
   const [msg, setMsg] = useState({ 'type': 'none', 'message': '' })
@@ -16,7 +19,7 @@ function App() {
       if (result.detail) {
         setMsg({ 'type': 'error', 'message': result.detail });
       } else {
-        setMsg({ 'type': 'success', 'message': 'Successfully retrieved books.' })
+        setMsg({ 'type': 'info', 'message': 'Successfully retrieved books.' })
         setBooks(result)
       }
     } catch (error) {
@@ -39,11 +42,22 @@ function App() {
             <BookTable book_list={books} />
           </div>
         </div>
-        <button onClick={() => {
-          setMsg({ 'type': 'success', 'message': "Book added to library!" });
-        }}>
-          Test
-        </button>
+        <BookModal action='add' id="book_modal" />
+        <div className="relative">
+          <button className="z-20 text-white flex flex-col shrink-0 grow-0 justify-around 
+                  fixed bottom-0 right-0 right-5 rounded-lg
+                  mr-1 mb-5 lg:mr-5 lg:mb-5 xl:mr-10 xl:mb-10"
+            onClick={
+              () => {
+                document.querySelector('#book_modal')?.classList.remove('hidden');
+                setMsg({ 'type': 'success', 'message': 'New book added!' })
+              }
+            }>
+            <div className="px-4 py-3 rounded-full border-4 border-white bg-gray-600">
+              <FontAwesomeIcon icon={faPlus} size="xl" color="#fff" />
+            </div>
+          </button>
+        </div>
       </div >
     </>
   )
--- client/src/components/BookModal.tsx
+++ client/src/components/BookModal.tsx
@@ -1,63 +1,85 @@
+import { useState } from "react"
+
 interface ModalProps {
   show?: boolean
-  action: 'add' | 'edit' | 'delete'
+  id: string
+  book_id?: number
+  action: 'add' | 'edit'
 }
 
 export default function BookModal(props: ModalProps) {
+  const [is_available, setAvailable] = useState(true)
+
+
+  function toggle_switch() {
+    let my_switch = document.querySelector('#switch')
+    setAvailable(!is_available)
+
+    my_switch?.classList.toggle('left-1');
+    my_switch?.classList.toggle('bg-indigo-500');
+    document.querySelector('#bg_switch')?.classList.toggle('bg-indigo-200')
+
+    my_switch?.classList.toggle('right-1');
+    my_switch?.classList.toggle('bg-amber-500');
+    document.querySelector('#bg_switch')?.classList.toggle('bg-amber-200')
+
+    if (my_switch) {
+      my_switch.textContent = my_switch.textContent === "No disponible" ? "Disponible" : "No disponible"
+    }
+  }
+
   return (
-    <div id="book-modal"
-      className={"absolute h-screen w-screen m-0 p-0 left-0 top-0 bg-gray-600/60 z-40" + (props.show ? "" : " hidden")} >
-      <div className="md:w-3/4 lg:w-1/3 mx-auto py-4 px-8 bg-gray-200 center md:my-8">
+    <div id={props.id}
+      className={"absolute h-screen w-screen m-0 p-0 flex justify-center items-center left-0 top-0 bg-gray-600/60 z-40" + (props.show ? "" : " hidden")} >
+      <div className="md:max-h-5/6 mb-auto md:my-auto md:w-3/4 lg:w-1/3 mx-auto px-8 py-4 bg-gray-200">
         <form action="">
-          <div className="py-8 mb-8 border-gray-300 border-b-2">
-            <h1 className="text-center text-4xl md:text-5xl font-semibold tracking-widest text-gray-700 rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline">
-              BibliotecaVM v2
-            </h1>
-          </div>
-          <div className="flex items-center mb-5">
-            <label className="inline-block w-20 mr-6 text-right font-bold text-gray-600">
+          <div
+            id="bg_switch"
+            onClick={(e) => {
+              e.preventDefault()
+              toggle_switch()
+            }}
+            className="mx-8 rounded border-b-2 border-gray-400 h-10 mt-4 mb-5 flex p-1 relative items-center bg-indigo-200">
+            <span
+              id="switch"
+              className="bg-indigo-500 drop-shadow-sm text-gray-200 flex items-center justify-center w-1/2 rounded h-8 transition-all ease-in duration-300 top-1 absolute left-1 ">
               Disponible
-            </label>
-            <div className="flex items-center justify-center">
-              <input
-                type="checkbox"
-                className="appearance-none w-9 focus:outline-none checked:bg-blue-300 h-5 bg-gray-300 rounded-full before:inline-block before:rounded-full before:bg-blue-500 before:h-4 before:w-4 checked:before:translate-x-full shadow-inner transition-all duration-300 before:ml-0.5"
-              />
-            </div>
+            </span>
+            <input id="is_available" type="checkbox" checked={is_available} className="hidden" />
           </div>
           <div className="flex items-center mb-5">
             <label htmlFor='title' className="inline-block w-20 mr-6 text-right font-bold text-gray-600">
               Titulo
             </label>
             <input type="text" name="title" id="title" placeholder="Título del libro"
-              className="flex-1 px-2 py-2 border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none" />
+              className="flex-1 px-2 py-2 border-b-2 border-gray-400 focus:border-sky-500 text-gray-600 placeholder-gray-400 outline-none" />
           </div>
           <div className="flex items-center mb-5">
             <label htmlFor='author' className="inline-block w-20 mr-6 text-right font-bold text-gray-600">
               Autor
             </label>
             <input type="text" name="author" id="author" placeholder="Autor del libro"
-              className="flex-1 px-2 py-2 border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none" />
+              className="flex-1 px-2 py-2 border-b-2 border-gray-400 focus:border-sky-500 text-gray-600 placeholder-gray-400 outline-none" />
           </div>
           <div className="flex items-center mb-5">
-            <label htmlFor='editorial' className="inline-block w-20 mr-6 text-right font-bold text-gray-600">
+            <label htmlFor='isbn' className="inline-block w-20 mr-6 text-right font-bold text-gray-600">
               ISBN
             </label>
-            <input type="text" name="editorial" id="editorial" placeholder="Editorial del libro"
-              className="flex-1 px-2 py-2 border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none" />
+            <input type="text" name="isbn" id="isbn" placeholder="ISBN del libro"
+              className="flex-1 px-2 py-2 border-b-2 border-gray-400 focus:border-sky-500 text-gray-600 placeholder-gray-400 outline-none" />
           </div>
           <div className="flex items-center mb-5">
             <label htmlFor='editorial' className="inline-block w-20 mr-6 text-right font-bold text-gray-600">
               Editorial
             </label>
             <input type="text" name="editorial" id="editorial" placeholder="Editorial del libro"
-              className="flex-1 px-2 py-2 border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none" />
+              className="flex-1 px-2 py-2 border-b-2 border-gray-400 focus:border-sky-500 text-gray-600 placeholder-gray-400 outline-none" />
           </div>
           <div className="flex items-center mb-5">
             <label className="inline-block w-20 mr-6 text-right font-bold text-gray-600">
               Tipo
             </label>
-            <select className="flex-1 p-2 border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none">
+            <select className="flex-1 p-2 border-b-2 border-gray-400 focus:border-sky-500 text-gray-600 placeholder-gray-400 outline-none bg-white">
               <option>Selecciona</option>
               <option>Pasta dura</option>
               <option>Pasta blanda</option>
@@ -72,20 +94,25 @@ export default function BookModal(props: ModalProps) {
               name="synopsis"
               id="synopsis"
               placeholder="Sinopsis del libro"
-              className="flex-1 px-2 py-2 border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none resize-none"
-              rows={4}
+              className="flex-1 px-2 py-2 border-b-2 border-gray-400 focus:border-sky-500 text-gray-600 placeholder-gray-400 outline-none resize-none"
+              rows={3}
             ></textarea>
           </div>
           <div className="flex items-center mb-5">
             <label htmlFor='categories' className="inline-block w-20 mr-6 text-right font-bold text-gray-600">
               Categorías
             </label>
-            <input type="text" name="categories" id="categories" placeholder="Editorial del libro"
-              className="flex-1 px-2 py-2 border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none" />
+            <textarea
+              name="categories"
+              id="categories"
+              placeholder="Categorías separadas por comas"
+              className="flex-1 px-2 py-2 border-b-2 border-gray-400 focus:border-sky-500 text-gray-600 placeholder-gray-400 outline-none resize-none"
+              rows={2}
+            ></textarea>
           </div>
-          <div className="text-right pb-6 border-gray-300 border-b-2 mb-4">
+          <div className="text-right pb-6">
             <button className="py-3 px-8 text-gray-600 border-gray-600 border-2 hover:bg-red-400 hover:border-gray-200 hover:text-gray-200 font-bold ml-4">Cancelar</button>
-            <button className="py-3 px-8 bg-gray-600 hover:bg-gray-700 border-gray-600 border-2 text-white font-bold ml-4">Enviar</button>
+            <button className="py-3 px-8 bg-indigo-600 hover:bg-indigo-500 border-indigo-600 hover:border-indigo-500 border-2 text-white font-bold ml-4">Guardar</button>
           </div>
         </form>
       </div>
