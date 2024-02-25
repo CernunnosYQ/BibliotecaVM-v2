f1d9929396db8e58b01dd70fb12dbe22893e073a
Yair Quintana <yairquintanamx@gmail.com>
2024-02-25 06:53:50 -0600

    feat(crud): Add login function and 'Authorization' header to destructive ops requests (51 min)

--- client/src/utils/crud.ts
+++ client/src/utils/crud.ts
@@ -2,6 +2,7 @@ import { BookProps } from "../components/BookTable";
 
 const API_URL = 'http://localhost:8000/api'
 
+
 const getAllBooks = async (): Promise<{ success: boolean, data: BookProps[] }> => {
   try {
     const response = await fetch(`${API_URL}/get/books`)
@@ -33,10 +34,11 @@ const getBook = async (id: number): Promise<{ success: boolean, data: BookProps 
 
 const createNewBook = async (bookData: BookProps): Promise<{ success: boolean, detail: string }> => {
   try {
+    const access_token = localStorage.getItem('access_token') || ''
     const response = await fetch(`${API_URL}/create/book`, {
       method: 'POST',
       body: JSON.stringify(bookData),
-      headers: { 'Content-Type': 'application/json' },
+      headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${access_token}` },
     })
     const data = await response.json()
 
@@ -52,10 +54,11 @@ const createNewBook = async (bookData: BookProps): Promise<{ success: boolean, d
 
 const updateBook = async (book_data: BookProps): Promise<{ success: boolean; detail: string }> => {
   try {
+    const access_token = localStorage.getItem('access_token') || '';
     const response = await fetch(`${API_URL}/update/book/${book_data.id}`, {
       method: 'PUT',
       body: JSON.stringify(book_data),
-      headers: { 'Content-Type': 'application/json' },
+      headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${access_token}` },
     })
     const data = await response.json()
 
@@ -71,7 +74,11 @@ const updateBook = async (book_data: BookProps): Promise<{ success: boolean; det
 
 const deleteBook = async (id: number): Promise<{ success: boolean, detail: string }> => {
   try {
-    const response = await fetch(`${API_URL}/delete/book/${id}`, { method: 'DELETE' })
+    const access_token = localStorage.getItem('access_token') || ''
+    const response = await fetch(`${API_URL}/delete/book/${id}`, {
+      method: 'DELETE',
+      headers: { "Authorization": `Bearer ${access_token}` }
+    })
     const data = await response.json()
 
     if (response.status === 200) {
@@ -84,10 +91,29 @@ const deleteBook = async (id: number): Promise<{ success: boolean, detail: strin
   }
 }
 
+const login = async (username: string, password: string) => {
+  try {
+    const form_data = new FormData()
+    form_data.append('username', username)
+    form_data.append('password', password)
+
+    const response = await fetch(`${API_URL}/login`, {
+      method: 'POST',
+      body: form_data,
+    })
+
+    const data = await response.json()
+    localStorage.setItem('access_token', data.access_token)
+  } catch (error) {
+    console.error("Error fetching token: ", error)
+  }
+}
+
 export {
   getAllBooks,
   getBook,
   createNewBook,
   updateBook,
   deleteBook,
+  login
 };
